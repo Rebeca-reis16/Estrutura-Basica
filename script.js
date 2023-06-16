041baedbc4927b7447f1287854ed711d698e3e84
Rebeca-reis16 <rebecatr6858@gmail.com>
2023-06-16 12:05:29 -0300

    Criação dos arquivos

---
+++ script.js
@@ -0,0 +1,10 @@
+const ok= document.getElementById('Ok');
+const input= document.getElementById('input');
+
+
+ok.onclick =()=>{
+    let tmp='';
+    tmp = input.value
+    alert('O nome digitado foi:' + tmp)
+}
+
